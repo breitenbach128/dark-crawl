b91fe0e52f894dd8a7ef5c723049bb02824ca95e
Benjamin Bach <breitenbach128@hotmail.com>
2026-05-31 21:56:04 -0500

    Working on Card draw and discard animations

--- Weapons/attack.gd
+++ Weapons/attack.gd
@@ -3,9 +3,6 @@ class_name Attack
 
 @export var damage : int = 1
 
-func _on_timer_timeout() -> void:
-	queue_free()
-
 func attack(attack_damage):
 	#For multiplayer, I'll eventually need player ID to ensure they get the score
 	$Anim.play("attack")
@@ -16,3 +13,12 @@ func _on_body_entered(body: Node) -> void:
 		if body.health_component:
 			body.health_component.take_damage(damage)
 			call_deferred("queue_free")
+
+func _on_anim_animation_finished() -> void:
+	print("Anim finished ", name)
+	queue_free()
+
+
+func _on_lifespan_timeout() -> void:
+	print("Attack Timeout ", name)
+	queue_free()
