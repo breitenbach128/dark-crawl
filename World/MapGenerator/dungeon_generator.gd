7573cfc08823fabef883a99527ea0feb1aa8582c
Benjamin Bach <breitenbach128@hotmail.com>
2026-06-09 21:45:36 -0500

    More state machine stuff and some attack code for enemies

--- World/MapGenerator/dungeon_generator.gd
+++ World/MapGenerator/dungeon_generator.gd
@@ -6,7 +6,7 @@ var astar_grid : AStarGrid2D = AStarGrid2D.new()
 
 var map_size : Rect2i = Rect2i(0,0,10,10)
 var map_tiles : Array = []
-var tile_size: Vector2i = Vector2i(5,5) # 5m x 5m
+var tile_size: Vector2i = Vector2i(1,1) # 1m x 1m
 var room_list : Array[RoomData] = []
 var room_attempt_count : int = 3
 
@@ -14,7 +14,7 @@ func init_grid() -> void:
 	astar_grid.region = map_size
 	astar_grid.cell_size = tile_size
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER # Disable diagonals
-	astar_grid.heuristic = AStarGrid2D.HEURISTIC_MANHATTAN     # Best for 4-way grids
+	astar_grid.default_estimate_heuristic  = AStarGrid2D.HEURISTIC_MANHATTAN
 	astar_grid.update() #Required after change
 
 func set_cell(cell: CellData):
@@ -26,29 +26,60 @@ func _ready() -> void:
 	init_grid()	
 	#Create map of empty tiles
 	for j in range(0,map_size.size.y):
+		var row_array = []
 		for i in range(0,map_size.size.x):
 			var cell = CellData.new()
 			cell.type = CellData.TILETYPE.EMPTY
 			cell.position = Vector2i(i,j)
-			map_tiles[i][j] = cell
+			#print(i," ",j)
+			row_array.append(cell)
+		map_tiles.append(row_array)
+	
+	create_rooms()
 
 func create_rooms():
 	#Create X rooms of various sizes. 
-	for i in range(0,3):
+	for rc in range(0,room_attempt_count):
+		
 		#Create a room of random size, no bigger than half the map
 		var room = RoomData.new()
-		room.room_size = Vector2i(randi_range(1,floor(map_size.size.x/2)),randi_range(1,floor(map_size.size.y/2)))
+		room.rect = Rect2i(Vector2i(0,0),Vector2i(randi_range(1,floor(map_size.size.x/2)),randi_range(1,floor(map_size.size.y/2))))
+		#print("Creating Room ", rc, " size: ", room.rect)
 		var check_limit : int = 50 #Kill the loop after 50 attempts
 		var position_found : bool = false
 		while check_limit > 0 && !position_found:
 			check_limit -= 1 #Safety Net
-			room.position = Vector2i(randi_range(0,map_size.size.x),randi_range(0,map_size.size.y))
-			is_room_position_valid(room)
-	#carve them into the space
-	
+			#try a position
+			room.rect.position.x = randi_range(0,map_size.size.x)
+			room.rect.position.y = randi_range(0,map_size.size.y)
+			if is_room_position_valid(room):
+				room_list.append(room)
+				position_found = true
+				print("Room position was valid: ",rc," ", room.rect)
+				#carve them into the space
+				for ry in room.rect.size.y:
+					for rx in room.rect.size.x:
+						map_tiles[rx+room.rect.position.x][ry+room.rect.position.y].type = CellData.TILETYPE.TILE
+						set_cell(map_tiles[rx][ry])
+				
+				
+	for j in range(0,map_size.size.y):
+		var strrow = ""
+		for i in range(0,map_size.size.x):
+			strrow+= str(map_tiles[i][j].type," , ")
+		print("ROW:", strrow)
 	#Pick exits on each room and connect to the closest room exit
 	pass
 
 func is_room_position_valid(room : RoomData):
-	
+	if(room.rect.position.x > 0 && 
+	room.rect.position.y > 0 && 
+	room.rect.position.x+room.rect.size.x < map_size.size.x &&
+	room.rect.position.y+room.rect.size.y < map_size.size.y):
+		#Within Bounds of map
+		for r in room_list:
+			#Ensure it does not intersect another room. It can touch
+			if room.rect.intersects(r.rect):
+				return false
+		return true
 	return false
--- World/MapGenerator/room_data.gd
+++ World/MapGenerator/room_data.gd
@@ -2,6 +2,5 @@ extends Resource
 class_name RoomData
 
 var room_id : int = -1 #ID of the room for reference
-var position : Vector2i = Vector2i(0,0)
-var room_size : Vector2i = Vector2i(4,4)
+var rect : Rect2i = Rect2i(0,0,1,1)
 var exit_locations : Array[Vector2i] = []
