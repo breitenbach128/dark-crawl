8c209cbc4c56be243b01d41a48753dcc896c6736
Benjamin Bach <breitenbach128@hotmail.com>
2026-07-03 13:49:37 -0500

    Working on MP

--- Statemachine/enemy_death.gd
+++ Statemachine/enemy_death.gd
@@ -2,7 +2,7 @@ extends State
 class_name StateEnemyDeath
 
 @export var enemy : Enemy
-
+var death_time : float = 0.0
 
 func death_animation_complete():
 	enemy.queue_free()
@@ -11,3 +11,8 @@ func Enter():
 	#print("Enter State Enemy Death")
 	if enemy.animation_tree:
 		enemy.behavior = "Death"
+
+func Update(delta):
+	death_time += delta
+	if death_time > 1:
+		enemy.queue_free()
