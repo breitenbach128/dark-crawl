a3d38ceaa8ce88d2002d683f2e5b9ee7336a07c8
Benjamin Bach <breitenbach128@hotmail.com>
2026-07-11 22:20:14 -0500

    Working on MP

--- World/Props/Crate/crate_break.gd
+++ World/Props/Crate/crate_break.gd
@@ -2,10 +2,15 @@ extends Node3D
 
 func _ready() -> void:
 	print("Crate Debris")
-	var tween = create_tween()
-	#tween.tween_property(self, "modulate:a", 0.0, 3.0)
-	#Cant do alpha like this in 3d. Need to do albedo and transparency
-	tween.tween_interval(2.5) # Pauses the sequence for 0.5 seconds
+	var tween = create_tween()	
+	tween.set_parallel(true)
+	var all_meshes: Array[Node] = find_children("*", "MeshInstance3D")
+	for mesh : MeshInstance3D in all_meshes:
+		var material = mesh.get_active_material(0)
+		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_DEPTH_PRE_PASS
+		material.depth_draw_mode = BaseMaterial3D.DEPTH_DRAW_ALWAYS
+		tween.tween_property(material, "albedo_color:a", 0.0, 10.0) # Fades to 0 alpha over 1.0 seconds
+			
 	tween.finished.connect(remove_debris)
 
 func remove_debris():
